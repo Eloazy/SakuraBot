c65ef76d290ba51c552eed5baa6013459e11b8b3
Yume <149901413+Eloazy@users.noreply.github.com>
2025-01-07 22:33:56 -0300

    Update apply.js

--- sakura/commandsStorage/apply.js
+++ sakura/commandsStorage/apply.js
@@ -2,41 +2,57 @@ const { ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle } = req
 module.exports = async function(interaction) {
 
   const modal = new ModalBuilder()
-			.setCustomId('apply')
-			.setTitle('Lunar Apply')
-  
-		const nameInput = new TextInputBuilder()
-			.setCustomId('nameInput')
-			.setLabel("input your ROBLOX and DISCORD user")
-			.setStyle(TextInputStyle.Short)
-	                .setPlaceholder('example: robloxUser / discordUser')
+	.setCustomId('apply')
+	.setTitle('Lunar Apply')
 
-		const whyJoin = new TextInputBuilder()
-			.setCustomId('whyJoin')
-			.setLabel("why you want join on Lunar Covenant")
-			.setStyle(TextInputStyle.Paragraph);
+	const nameInput = new TextInputBuilder()
+		.setCustomId('nameInput')
+		.setLabel("input your ROBLOX user")
+		.setStyle(TextInputStyle.Short)
+		.setPlaceholder('example: yumekko54')
 
-	       const killInput = new TextInputBuilder()
-			.setCustomId('killInput')
-			.setLabel("input your kills in-game")
-			.setStyle(TextInputStyle.Short)
+	const whyJoin = new TextInputBuilder()
+		.setCustomId('whyJoin')
+		.setLabel("why you want join on Lunar Covenant")
+		.setStyle(TextInputStyle.Paragraph);
+
+	const killInput = new TextInputBuilder()
+		.setCustomId('killInput')
+		.setLabel("input your kills in-game")
+		.setStyle(TextInputStyle.Short)
 	       
-	      const deathInput = new TextInputBuilder()
-			.setCustomId('deathInput')
-			.setLabel("input your deaths in-game")
-			.setStyle(TextInputStyle.Short)
+	const deathInput = new TextInputBuilder()
+		.setCustomId('deathInput')
+		.setLabel("input your deaths in-game")
+		.setStyle(TextInputStyle.Short)
 
-	      const levelInput = new TextInputBuilder()
-			.setCustomId('levelInput')
-			.setLabel("input your level in-game")
-			.setStyle(TextInputStyle.Short)
+	const levelInput = new TextInputBuilder()
+		.setCustomId('levelInput')
+		.setLabel("input your level in-game")
+		.setStyle(TextInputStyle.Short)
 	
-		const firstActionRow = new ActionRowBuilder().addComponents(nameInput);
-		const secondActionRow = new ActionRowBuilder().addComponents(whyJoin);
-                const tirthActionRow = new ActionRowBuilder().addComponents(killInput);
-                const fourthActionRow = new ActionRowBuilder().addComponents(deathInput)
-	        const fifthActionRow = new ActionRowBuilder().addComponents(levelInput)
-		modal.addComponents(firstActionRow, secondActionRow, tirthActionRow, fourthActionRow, fifthActionRow);
+	const firstActionRow = new ActionRowBuilder().addComponents(nameInput);
+	const secondActionRow = new ActionRowBuilder().addComponents(whyJoin);
+        const tirthActionRow = new ActionRowBuilder().addComponents(killInput);
+        const fourthActionRow = new ActionRowBuilder().addComponents(deathInput)
+	const fifthActionRow = new ActionRowBuilder().addComponents(levelInput)
+	modal.addComponents(firstActionRow, secondActionRow, tirthActionRow, fourthActionRow, fifthActionRow);
   
-		await interaction.showModal(modal);
+	await interaction.showModal(modal);
+	const filter = (interaction) => interaction.customId === 'apply'
+
+        interaction.awaitModalSubmit({ filter, time: 30_000 }).then((interaction) => {
+		modalInteraction.reply({ content: 'apply submited', ephemeral: true })
+	})
+}      
+
+async function(interaction, name, whyJoin, Kills, Deaths, Level) {
+	const apply = new EmbedBuilder()
+		.setColor("FFA6C9")
+		.setTitle(`<@${interaction.user.id}> apply`)
+		.setDescription('why join in lunar?\n'+whyJoin)
+		.setTimestamp()
+		.setFooter({ text: 'SakuraSystem 3.0 - apply' })
+		
+		await interaction.guild.channels.cache.get(channel.id).send({ embeds: [apply] })
 }
