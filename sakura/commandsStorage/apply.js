23845648040f39fae74faa70e8bd6254970375ac
Yume <149901413+Eloazy@users.noreply.github.com>
2025-01-08 18:28:47 -0300

    Correction bugs

--- sakura/commandsStorage/apply.js
+++ sakura/commandsStorage/apply.js
@@ -72,36 +72,25 @@ async function embedMaker(interaction, name, whyJoin, kill, death, level) {
 		.setFooter({ text: 'SakuraSystem 3.0 - apply' })
 	
 	const approved = new ButtonBuilder()
-			.setCustomId('approved')
-			.setLabel('approve this apply')
-			.setStyle(ButtonStyle.Success);
+		.setCustomId('approved')
+		.setLabel('approve this apply')
+		.setStyle(ButtonStyle.Success)
+		.setDisabled(true)
 	
 	const denied = new ButtonBuilder()
 		.setCustomId('Denied')
 		.setLabel('deny this apply')
-		.setStyle(ButtonStyle.Secondary);
+		.setStyle(ButtonStyle.Secondary)
+	        .setDisabled(true)
 	
 	const blacklisted = new ButtonBuilder()
-			.setCustomId('Blacklist')
-			.setLabel('blacklist this member')
-			.setStyle(ButtonStyle.Danger);
+		.setCustomId('Blacklist')
+		.setLabel('blacklist this member')
+		.setStyle(ButtonStyle.Danger)
+		.setDisabled(true)
 	
 	const row = new ActionRowBuilder()
-			.addComponents(approved, denied, blacklisted);
+		.addComponents(approved, denied, blacklisted);
 
-	const response = await interaction.guild.channels.cache.get("1326365936046968856").send({ embeds: [apply], components: [row] })
-
-	const collectorFilter = i => i.user.id === interaction.user.id;
-	
-	try {
-		const button = await response.awaitMessageComponent({ filter: collectorFilter, time: 60_000 });
-		
-		if (button.customId === 'approved') {
-			await interaction.guild.channels.cache.get("1326365936046968856").send('apply approved');
-		} else if (confirmation.customId === 'denied') {
-			await response.update({ content: 'Action cancelled', components: [] })
-		}
-	} catch (e) {
-		await console.error(e)//interaction.editReply({ content: 'Confirmation not received within 1 minute, cancelling', components: [] });
-	}
+	await interaction.guild.channels.cache.get("1326365936046968856").send({ embeds: [apply], components: [row] })
 }
