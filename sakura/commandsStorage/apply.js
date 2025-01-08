1283df28f63fc8d2f4cb9fa3f70d95ec569ebc20
Yume <149901413+Eloazy@users.noreply.github.com>
2025-01-08 17:30:01 -0300

    Update apply.js

--- sakura/commandsStorage/apply.js
+++ sakura/commandsStorage/apply.js
@@ -1,4 +1,4 @@
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle } = require("discord.js")
+const { ButtonBuilder, ButtonStyle, EmbedBuilder, ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle } = require("discord.js")
 module.exports = async function(interaction) {
 
   const modal = new ModalBuilder()
